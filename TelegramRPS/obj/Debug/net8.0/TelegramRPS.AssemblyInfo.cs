b96d5bfd939a2c50a8a43079c894ddf1cfedd39f
PC <mega.kunis@gmail.com>
2025-05-27 11:55:28 +0300

    add other proj

--- TelegramRPS/obj/Debug/net8.0/TelegramRPS.AssemblyInfo.cs
+++ TelegramRPS/obj/Debug/net8.0/TelegramRPS.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TelegramRPS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+58a5734179be86932902578fa58a8145b9a54c88")]
 [assembly: System.Reflection.AssemblyProductAttribute("TelegramRPS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TelegramRPS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
